203d871f8d3385a95855e9b735e5f0057a107852
Ben Simpson <thehoagie@gmail.com>
2024-08-15 15:12:48 -0400

    Change result parsing for Neptune format

--- lib/neo4j/http/results.rb
+++ lib/neo4j/http/results.rb
@@ -3,26 +3,69 @@ require "forwardable"
 module Neo4j
   module Http
     class Results
-      # Example result set:
-      # [{"columns"=>["n"],
-      # "data"=>
-      # [{"row"=>[{"name"=>"Foo", "uuid"=>"8c7dcfda-d848-4937-a91a-2e6debad2dd6"}],
-      #   "meta"=>[{"id"=>242, "type"=>"node", "deleted"=>false}]}]}]
-      #
+      # And return result from this gem
+      # [{"u1"=>
+      #   {"specialty"=>"Colon & Rectal Surgery",
+      #     "city"=>"Staten Island",
+      #     "credentials"=>"DO",
+      #     "profile_url"=>"https://doximity.qa-skaur-1.doximity-staging.services/profiles/f3dff658-3c79-4227-878b-8cf56a6d32c9",
+      #     "last_name"=>"test",
+      #     "lon"=>-74.1780014038086,
+      #     "state"=>"New York",
+      #     "uuid"=>"f3dff658-3c79-4227-878b-8cf56a6d32c9",
+      #     "legacy_elasticsearch_user_id"=>5532386,
+      #     "first_name"=>"samm",
+      #     "lat"=>40.61940002441406,
+      #     "subspecialties"=>"General Colon & Rectal Surgery, Clinical Pharmacology, Clinical Informatics",
+      #     "_neo4j_meta_data"=>{"id"=>0, "type"=>"node", "deleted"=>false}}},
+      # {"u1"=>
+      #   {"specialty"=>"Other MD/DO",
+      #     "city"=>"Marina",
+      #     "profile_url"=>"https://doximity.qa-skaur-1.doximity-staging.services/profiles/f40908ec-2f4e-4a49-97bc-68e70f341f9d",
+      #     "credentials"=>"MD",
+      #     "last_name"=>"kaur",
+      #     "lon"=>-121.80216979980469,
+      #     "state"=>"California",
+      #     "uuid"=>"f40908ec-2f4e-4a49-97bc-68e70f341f9d",
+      #     "legacy_elasticsearch_user_id"=>5532349,
+      #     "first_name"=>"sandip",
+      #     "lat"=>36.68439865112305,
+      #     "subspecialties"=>"Other MD/DO",
+      #     "_neo4j_meta_data"=>{"id"=>1, "type"=>"node", "deleted"=>false}}}]
+
+      # Neptune Example result set
+      # {
+      #   "results": [{
+      #       "n1": {
+      #         "~id": "7b3caa7b-4187-4689-be1f-8d8c924a23df",
+      #         "~entityType": "node",
+      #         "~labels": ["User"],
+      #         "~properties": {
+      #           "uuid": "abc123"
+      #         }
+      #       }
+      #     }, {
+      #       "n1": {
+      #         "~id": "28b2bc2a-03d4-415d-9f37-bd4b9420e449",
+      #         "~entityType": "node",
+      #         "~labels": ["User"],
+      #         "~properties": {
+      #           "uuid": "def456"
+      #         }
+      #       }
+      #     }]
       def self.parse(results)
-        columns = results["columns"]
-        data = results["data"]
+        results.map do |result|
+          entries = {}
+          result.each_pair do |key, value|
+            meta = {
+              "id" => value["~id"],
+              "type" => value["~entityType"],
+            }
 
-        data.map do |result|
-          row = result["row"] || []
-          meta = result["meta"] || []
-          compacted_data = row.each_with_index.map do |attributes, index|
-            row_meta = meta[index] || {}
-            attributes["_neo4j_meta_data"] = row_meta if attributes.is_a?(Hash)
-            attributes
+            entries[key] = value["~properties"].merge({ "_neo4j_meta_data" => meta })
           end
-
-          columns.zip(compacted_data).to_h.with_indifferent_access
+          entries
         end
       end
     end
