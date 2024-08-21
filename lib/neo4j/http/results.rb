98971913fdb2988ec34256abd7c623818809410c
Ben Simpson <thehoagie@gmail.com>
2024-08-21 11:40:19 -0400

    Change how we parse ~properties
    In Neo4j the properties of a node or relationship were at the level as
    the id, and type keys.
    
    In Neptune these properties are stored in a hash under a "~properties"
    key. We hoist these values to be at the same level as Neo4j so our
    query results are formatted the same way

--- lib/neo4j/http/results.rb
+++ lib/neo4j/http/results.rb
@@ -3,72 +3,23 @@ require "forwardable"
 module Neo4j
   module Http
     class Results
-      # And return result from this gem
-      # [{"u1"=>
-      #   {"specialty"=>"Colon & Rectal Surgery",
-      #     "city"=>"Staten Island",
-      #     "credentials"=>"DO",
-      #     "profile_url"=>"https://doximity.qa-skaur-1.doximity-staging.services/profiles/f3dff658-3c79-4227-878b-8cf56a6d32c9",
-      #     "last_name"=>"test",
-      #     "lon"=>-74.1780014038086,
-      #     "state"=>"New York",
-      #     "uuid"=>"f3dff658-3c79-4227-878b-8cf56a6d32c9",
-      #     "legacy_elasticsearch_user_id"=>5532386,
-      #     "first_name"=>"samm",
-      #     "lat"=>40.61940002441406,
-      #     "subspecialties"=>"General Colon & Rectal Surgery, Clinical Pharmacology, Clinical Informatics",
-      #     "_neo4j_meta_data"=>{"id"=>0, "type"=>"node", "deleted"=>false}}},
-      # {"u1"=>
-      #   {"specialty"=>"Other MD/DO",
-      #     "city"=>"Marina",
-      #     "profile_url"=>"https://doximity.qa-skaur-1.doximity-staging.services/profiles/f40908ec-2f4e-4a49-97bc-68e70f341f9d",
-      #     "credentials"=>"MD",
-      #     "last_name"=>"kaur",
-      #     "lon"=>-121.80216979980469,
-      #     "state"=>"California",
-      #     "uuid"=>"f40908ec-2f4e-4a49-97bc-68e70f341f9d",
-      #     "legacy_elasticsearch_user_id"=>5532349,
-      #     "first_name"=>"sandip",
-      #     "lat"=>36.68439865112305,
-      #     "subspecialties"=>"Other MD/DO",
-      #     "_neo4j_meta_data"=>{"id"=>1, "type"=>"node", "deleted"=>false}}}]
-
-      # Neptune Example result set
-      # {
-      #   "results": [{
-      #       "n1": {
-      #         "~id": "7b3caa7b-4187-4689-be1f-8d8c924a23df",
-      #         "~entityType": "node",
-      #         "~labels": ["User"],
-      #         "~properties": {
-      #           "uuid": "abc123"
-      #         }
-      #       }
-      #     }, {
-      #       "n1": {
-      #         "~id": "28b2bc2a-03d4-415d-9f37-bd4b9420e449",
-      #         "~entityType": "node",
-      #         "~labels": ["User"],
-      #         "~properties": {
-      #           "uuid": "def456"
-      #         }
-      #       }
-      #     }]
       def self.parse(results)
-        results.map do |result|
-          entries = {}
-          result.each_pair do |key, value|
-            if value.is_a?(Hash)
-              meta = {
-                "id" => value["~id"],
-                "type" => value["~entityType"],
-              }
-              entries[key] = (value["~properties"] || {}).merge({ "_neo4j_meta_data" => meta }).with_indifferent_access
-            else
-              entries[key] = value
-            end
+        if results.is_a?(Array)
+          # Recuse on arrays
+          return results.map { |a| self.parse(a) }
+        elsif results.is_a?(Hash)
+          # Recurse on hashes
+          # Hoist ~properties key
+          new_hash = {}
+          results = results.except("~properties").merge(results["~properties"]) if results.key?("~properties")
+
+          results.each_pair do |k,v|
+            new_hash[k] = self.parse(v)
           end
-          entries
+          return new_hash
+        else
+          # Primative value
+          return results
         end
       end
     end
---
+++ spec/neo4j/http/results_spec.rb
@@ -0,0 +1,146 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+# Examples taken from
+# https://docs.aws.amazon.com/neptune/latest/userguide/access-graph-opencypher-queries.html
+RSpec.describe Neo4j::Http::Results, type: :uses_neo4j do
+  it "returns node response" do
+    results = JSON.parse %q(
+      {
+        "results": [
+          {
+            "a": {
+              "~id": "22",
+              "~entityType": "node",
+              "~labels": [
+                "airport"
+              ],
+              "~properties": {
+                "desc": "Seattle-Tacoma",
+                "lon": -122.30899810791,
+                "runways": 3,
+                "type": "airport",
+                "country": "US",
+                "region": "US-WA",
+                "lat": 47.4490013122559,
+                "elev": 432,
+                "city": "Seattle",
+                "icao": "KSEA",
+                "code": "SEA",
+                "longest": 11901
+              }
+            }
+          }
+        ]
+      }
+    )
+
+    output = described_class.parse(results["results"])
+    expected = {
+      "desc" => "Seattle-Tacoma",
+      "lon" => -122.30899810791,
+      "runways" => 3,
+      "type" => "airport",
+      "country" => "US",
+      "region" => "US-WA",
+      "lat" => 47.4490013122559,
+      "elev" => 432,
+      "city" => "Seattle",
+      "icao" => "KSEA",
+      "code" => "SEA",
+      "longest" => 11901,
+      "~id" => "22",
+      "~entityType" => "node",
+      "~labels" => [
+        "airport"
+      ],
+    }
+    expect(output[0]["a"]).to match(expected)
+  end
+
+  it "returns relationship response" do
+    results = JSON.parse %q(
+      {
+        "results": [
+          {
+            "r": {
+              "~id": "7389",
+              "~entityType": "relationship",
+              "~start": "22",
+              "~end": "151",
+              "~type": "route",
+              "~properties": {
+                "dist": 956
+              }
+            }
+          }
+        ]
+      }
+    )
+
+    output = described_class.parse(results["results"])
+    expected = {
+     "dist"=>956,
+     "~end" => "151",
+     "~entityType" => "relationship",
+     "~id" => "7389",
+     "~start" => "22",
+     "~type" => "route",
+    }
+    expect(output[0]["r"]).to eq(expected)
+  end
+
+  it "returns value response" do
+    results = JSON.parse %q(
+      {
+        "results": [
+          {
+            "count(a)": 121
+          }
+        ]
+      }
+    )
+
+    output = described_class.parse(results["results"])
+    expect(output[0]["count(a)"]).to eq(121)
+  end
+
+  it "returns user network query results" do
+    results = [
+      {
+        "colleagues" => [
+          [{
+            "~id" => "457e8e96-3cbc-4122-97a5-4dc9fe32dbc4",
+            "~entityType" => "node",
+            "~labels" => ["User"],
+            "~properties" => {
+              "name" => "Dolores Abernathy", "uuid" => "USER", "specialty" => "Immunology", "city" => "Westworld", "state" => "Utah"
+            }
+          }, {
+            "~id" => "387fb59e-9e7d-4507-ba17-7de691d3e13e",
+            "~entityType" => "relationship",
+            "~start" => "457e8e96-3cbc-4122-97a5-4dc9fe32dbc4",
+            "~end" => "48e1f64c-48d2-421f-8b10-38ebd58df761",
+            "~type" => "COLLEAGUES_WITH",
+            "~properties" => {
+              "state" => "invited"
+            }
+          }, {
+            "~id" => "48e1f64c-48d2-421f-8b10-38ebd58df761",
+            "~entityType" => "node",
+            "~labels" => ["User"],
+            "~properties" => {
+              "name" => "Maeve Millay", "uuid" => "OTHER-USER", "specialty" => "Immunology", "city" => "Westworld", "state" => "Utah"
+            }
+          }]
+        ], "medschool_classmates" => nil, "co_residents" => nil, "co_workers" => nil, "investigator_of" => nil, "full_name" => "Maeve Millay", "co_fellows" => nil, "co_authors" => nil, "paschool_classmates" => nil, "user_uuid" => "OTHER-USER"
+      }
+    ]
+
+    output = described_class.parse(results)
+    expect(output[0]["colleagues"][0][0]["name"]).to eq("Dolores Abernathy")
+    expect(output[0]["colleagues"][0][1]["state"]).to eq("invited")
+    expect(output[0]["colleagues"][0][2]["name"]).to eq("Maeve Millay")
+  end
+end
